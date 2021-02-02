98fe3fc67479b063c93b71bf7ce4e44b07182744
Keith Middleton <keith.middleton@ontariotechu.net>
2021-02-02 01:28:09 -0500

    Part 2

---
+++ main/csci2020u/lab02/CSVDemo.java
@@ -0,0 +1,17 @@
+import java.io.Reader;
+import org.apache.commons.csv.CSVRecord;
+public class CSVDemo {
+	public static void main(String[] args) {
+		try {
+			Reader in = new FileReader("data.csv");
+			Iterable<CSVRecord> records =CSVFormat.DEFAULT.withHeader("First Name", "Last Name", "SID","Grade").parse(in);
+			for (CSVRecord record: records) {
+				String lastName = record.get("Last Name");
+				String firstName = record.get("First Name");
+				String grade = record.get("Grade");
+				System.out.println(firstName + " " + lastName + " (" +grade + ")");}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
